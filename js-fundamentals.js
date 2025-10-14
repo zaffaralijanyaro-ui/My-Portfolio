9cbff5d0ca6e880f65b2132b25209472d5ca58a4
Zafar Ahmed <zaffaralijanyaro@gmail.com>
2025-10-14 17:33:23 +0500

    this is my game project

---
+++ js-fundamentals.js
@@ -0,0 +1,30 @@
+// let name= prompt("enter your name :");
+// if (name==="Zafar Ali"){
+//     name="your are a good boy.";
+// }
+// else if (name==="dildar"){
+//     name="you have a good name.";
+// }
+// else{
+//     name="don't worry you also have a good name.";
+// }
+// console.log(name);
+
+let a=55;
+let b=44;
+let operator=prompt("enter your operator?");
+// switch (operator){
+//     case '+':
+//         console.log("a+b =", (a+b));
+//         break;
+//     case '-':
+//         console.log("a-b =", (a-b));
+//         break;
+//     case '/':
+//         console.log("a/b =", (a/b));
+// }
+let i=1;
+let tableN=prompt ("enter your table number:")
+for (i=1; i<=10; i++){
+    console.log(tableN, "x" , i, "=", (tableN*i));
+}
